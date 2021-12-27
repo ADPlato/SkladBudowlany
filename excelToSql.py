4aecf5d650d06c0d7ce4ce3861f0b34ec7796179
ADPlato <adamplato93@gmail.com>
2021-12-27 19:16:12 +0100

    add super() method, changes in excelToSql methods

--- excelToSql.py
+++ excelToSql.py
@@ -1,17 +1,39 @@
 import xlrd
 import os
+import sqlite3
 
 
 def getExcelDBPath():
     return os.path.abspath("db_xls.xls")
 
-
-def getDBExcel():
+def getExcelData(excelPath):
     productsDB = []
-    databaseExcel = xlrd.open_workbook(getExcelDBPath())
-    sheetExcel = databaseExcel.sheet_by_index(0)
+    excelWorkBook = xlrd.open_workbook(excelPath)
+    print(excelWorkBook.sheet_names())
+    sheetExcel = excelWorkBook.sheet_by_index(0)
     for row in range(sheetExcel.nrows):
         productsDB.append(sheetExcel.row_values(row))
     print(productsDB)
     return productsDB
+
+def createSqlTable(name):
+    pass
+
+def connectToSql(name):
+    pass
+
+def insertToSql(name,data):
+    pass
+
+def selectSqlData(name,conditions):
+    pass
+
+def deleteSqlData(name,conditions):
+    pass
+
+def updateSqlData(nama, data):
+    pass
+
+if __name__=="__main__":
+    excelFile=getExcelData(r"C:\Users\User\OneDrive\Pulpit\b.xls")
 
--- userGUI.py
+++ userGUI.py
@@ -1,9 +1,10 @@
+import excelToSql
 from excelToSql import getExcelDBPath
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 
 
-class mainGui():
+class mainGui(object):
     def __init__(self, master):
         self.master = master
         self.master.title("Porównywanie ofert")
@@ -14,7 +15,7 @@ class mainGui():
         self.dbMenu = tk.Menu(self.menuBar, tearoff=0)
         self.helpMenu = tk.Menu(self.menuBar, tearoff=0)
 
-        self.fileMenu.add_command(label="Nowy")
+        self.fileMenu.add_command(label="Nowy", command=self.newtable)
         self.fileMenu.add_command(label="Zapisz")
         self.fileMenu.add_command(label="Wczytaj")
         self.fileMenu.add_command(label="Drukuj")
@@ -35,11 +36,16 @@ class mainGui():
         self.master.config(menu=self.menuBar)
         self.dbFileSelected = getExcelDBPath()
 
+        #self.newTable = guiTable(guiWindow)
+
     def setDBLocalization(self):
         self.dbFileSelected = filedialog.askopenfilename()
 
     def checkDBLocalization(self):
         messagebox.showinfo("Ścieżka do bazy danych", self.dbFileSelected)
+
+    def getDBLocalization(self):
+        return self.dbFileSelected
 
     def printInfo(self):
         infoFile = open('info.txt', 'r', encoding='utf-8')
@@ -49,14 +55,19 @@ class mainGui():
         progInfo = ''.join(progInfo)
         messagebox.showinfo("Informacje", progInfo)
 
+    def newtable(self):
+        gui = guiTable(guiWindow)
 
 class guiTable(mainGui):
-    def __init__ (self, master):
+    def __init__(self, master):
+        super().__init__(master)
+        print(self.dbFileSelected)
         self.master = master
         self.style = ttk.Style()
         self.style.theme_use('clam')
 
         self.tree = ttk.Treeview(master, column=("Producent", "Produkt", "Cena"), show='headings')
+        self.tree.bind("<Button-3>", self.right_click)
 
         self.scrollbar = ttk.Scrollbar(self.master)
 
@@ -66,8 +77,9 @@ class guiTable(mainGui):
         self.tree.heading("#1", text="Producent")
         self.tree.heading("#2", text="Produkt")
         self.tree.heading("#3", text="Cena")
-        for x in range(5):
-            self.tree.insert('', 'end', text="1", values=(f'cos{x}', 'cos', 'cos'))
+
+        for x in range(50):
+            self.tree.insert('', 'end', text="1", values=[f'a {x}', f'b{x * x}', f'c{x + x}'])
 
         self.scrollbar.grid(column=1, row=0, sticky='ns')
         self.tree.grid(column=0, row=0, sticky='nsew')
@@ -75,10 +87,14 @@ class guiTable(mainGui):
         self.master.grid_columnconfigure(0, weight=1)
         self.master.grid_rowconfigure(0, weight=1)
 
+    def right_click(self, x):
+        selectedItem = self.tree.focus()
+        print(self.tree.item(selectedItem))
+        self.tree.delete(selectedItem)
+
 
 if __name__ == "__main__":
     guiWindow = tk.Tk()
-    mainGui(guiWindow)
-    guiTable(guiWindow)
-
+    #gui = mainGui(guiWindow)
+    gui = guiTable(guiWindow)
     guiWindow.mainloop()

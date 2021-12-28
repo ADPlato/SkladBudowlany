c28bb6c2e20abada6a0857f95ad0e2cf17c8a098
ADPlato <adamplato93@gmail.com>
2021-12-28 13:54:41 +0100

    Poprawiki na szybko

--- excelToSql.py
+++ excelToSql.py
@@ -1,17 +1,53 @@
 import xlrd
+import xlwt
 import os
+from datetime import date
+import sqlite3
 
+def saveToExcel(excelName,excelPath, data):
+    excelWorkBook = xlwt.Workbook()
+    excelWorkSheet= excelWorkBook.add_sheet("Sheet1")
+    for x in range(len(data)):
+        for y in range(len(data[x])):
+            excelWorkSheet.write(x,y,data[x][y])
+    today=date.today()
+    today=today.strftime("%Y_%m_%d_")
+    excelWorkBook.save(excelPath+'\\'+today+excelName+'.xls')
+
+def readFromExcel():
+    pass
 
 def getExcelDBPath():
     return os.path.abspath("db_xls.xls")
 
-
-def getDBExcel():
+def getExcelData(excelPath):
     productsDB = []
-    databaseExcel = xlrd.open_workbook(getExcelDBPath())
-    sheetExcel = databaseExcel.sheet_by_index(0)
+    excelWorkBook = xlrd.open_workbook(excelPath)
+    print(excelWorkBook.sheet_names())
+    sheetExcel = excelWorkBook.sheet_by_index(0)
     for row in range(sheetExcel.nrows):
         productsDB.append(sheetExcel.row_values(row))
     print(productsDB)
     return productsDB
 
+def createSqlTable(name):
+    pass
+
+def connectToSql(name):
+    pass
+
+def insertToSql(name,data):
+    pass
+
+def selectSqlData(name,conditions):
+    pass
+
+def deleteSqlData(name,conditions):
+    pass
+
+def updateSqlData(nama, data):
+    pass
+
+if __name__=="__main__":
+    pass
+    #excelFile=getExcelData(r"C:\Users\User\OneDrive\Pulpit\b.xls")
--- userGUI.py
+++ userGUI.py
@@ -1,22 +1,22 @@
-from excelToSql import getExcelDBPath
+from excelToSql import getExcelDBPath, saveToExcel
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
-
+import pickle
 
 class mainGui():
     def __init__(self, master):
         self.master = master
         self.master.title("Porównywanie ofert")
-        self.master.geometry("700x700")
+        self.master.geometry("700x400")
         self.menuBar = tk.Menu(self.master)
 
         self.fileMenu = tk.Menu(self.menuBar, tearoff=0)
         self.dbMenu = tk.Menu(self.menuBar, tearoff=0)
         self.helpMenu = tk.Menu(self.menuBar, tearoff=0)
 
-        self.fileMenu.add_command(label="Nowy")
-        self.fileMenu.add_command(label="Zapisz")
-        self.fileMenu.add_command(label="Wczytaj")
+        self.fileMenu.add_command(label="Nowy", command=self.newTable)
+        self.fileMenu.add_command(label="Zapisz", command = self.saveTable)
+        self.fileMenu.add_command(label="Wczytaj", command = self.loadTable)
         self.fileMenu.add_command(label="Drukuj")
         self.fileMenu.add_command(label="Zamknij", command=self.master.destroy)
 
@@ -35,6 +35,9 @@ class mainGui():
         self.master.config(menu=self.menuBar)
         self.dbFileSelected = getExcelDBPath()
 
+
+
+
     def setDBLocalization(self):
         self.dbFileSelected = filedialog.askopenfilename()
 
@@ -49,14 +52,40 @@ class mainGui():
         progInfo = ''.join(progInfo)
         messagebox.showinfo("Informacje", progInfo)
 
+    def newTable(self):
+        self.newTable=guiTable(self.master)
+
+
+    def saveTable(self):
+
+        self.excelFilePath=filedialog.askdirectory()
+        recordTable=[]
+        items=self.newTable.tree.get_children()
+        recordTable.append(self.newTable.tableColumns)
+
+        for x in items:
+            record=self.newTable.tree.item(x)
+            recordTable.append(record['values'])
+        saveToExcel('a',self.excelFilePath,recordTable)
+
+    def loadTable(self):
+        sub=subWindow(tk.Tk())
+
+class subWindow(mainGui):
+    def __init__(self, mast):
+        self.mast=mast
+        self.mast.title("dodaj")
+        self.mast.geometry("700x100")
 
 class guiTable(mainGui):
+
     def __init__ (self, master):
         self.master = master
         self.style = ttk.Style()
         self.style.theme_use('clam')
 
-        self.tree = ttk.Treeview(master, column=("Producent", "Produkt", "Cena"), show='headings')
+        self.tableColumns = ["Producent", "Produkt", "Cena"]
+        self.tree = ttk.Treeview(master, column=self.tableColumns, show='headings')
 
         self.scrollbar = ttk.Scrollbar(self.master)
 
@@ -66,19 +95,32 @@ class guiTable(mainGui):
         self.tree.heading("#1", text="Producent")
         self.tree.heading("#2", text="Produkt")
         self.tree.heading("#3", text="Cena")
-        for x in range(5):
-            self.tree.insert('', 'end', text="1", values=(f'cos{x}', 'cos', 'cos'))
 
-        self.scrollbar.grid(column=1, row=0, sticky='ns')
-        self.tree.grid(column=0, row=0, sticky='nsew')
+        for x in range(5):
+            self.tree.insert('', 'end', text="1", values=(f'{x}:{x}', f'{x}:{x+x}', f'{x}:{x*x}'))
+
+        self.scrollbar.grid(column=3, row=0, sticky='ns')
+        self.tree.grid(column=0, row=0, columnspan=3, sticky='nsew')
 
         self.master.grid_columnconfigure(0, weight=1)
         self.master.grid_rowconfigure(0, weight=1)
 
+        self.newButton = tk.Button(self.master,text='Dodaj')
+        self.deleteButton = tk.Button(self.master, text='Usuń', command = self.removeRow)
+
+        self.newButton.grid(column =0 , row=1, columnspan=4, sticky ='ew')
+        self.deleteButton.grid(column =0 , row=2, columnspan=4, sticky ='ew')
+
+    def removeRow(self):
+        selected=self.tree.focus()
+        if selected != '':
+            self.tree.delete(selected)
+
+
 
 if __name__ == "__main__":
     guiWindow = tk.Tk()
-    mainGui(guiWindow)
-    guiTable(guiWindow)
+    frame=mainGui(guiWindow)
+
 
     guiWindow.mainloop()
